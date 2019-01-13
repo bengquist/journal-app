3d39b68d7e6fe86203dcab3bf760abd77d92d0f4
bengquist <engquist_blake@yahoo.com>
2019-01-12 23:34:59 -0600

    remove sample code

--- client/components/navigation/Navigation.tsx
+++ client/components/navigation/Navigation.tsx
@@ -1,18 +1,6 @@
-import React from 'react';
-import {View, Text} from 'react-native';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 import Feed from '../feed/Feed';
 import Journal from '../journal/Journal';
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-}
 
 const AppNavigator = createStackNavigator(
   {
